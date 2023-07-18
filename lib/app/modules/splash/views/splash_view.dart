153e87c47267cc24eccb7b039afcf9ff1770080f
AbdQader <abd8alqader@gmail.com>
2023-07-18 20:09:21 +0300

    finishing the app

--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'package:get/get.dart';
 
 import '../../../../utils/constants.dart';
