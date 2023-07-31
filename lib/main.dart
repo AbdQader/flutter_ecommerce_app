0059691c9e0ca3fd5e87f0f656859da7f2a33a91
Emad Beltaje <emadbeltaje@gmail.com>
2023-07-31 09:14:48 +0300

    remove localization unused code ␡

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:get/get.dart';
 import 'app/data/local/my_shared_pref.dart';
 import 'app/routes/app_pages.dart';
 import 'config/theme/my_theme.dart';
-import 'config/translations/localization_service.dart';
 
 Future<void> main() async {
   // wait for bindings
@@ -39,8 +38,6 @@ Future<void> main() async {
           },
           initialRoute: AppPages.INITIAL, // first screen to show when app is running
           getPages: AppPages.routes, // app screens
-          locale: MySharedPref.getCurrentLocal(), // app language
-          translations: LocalizationService.getInstance(), // localization services in app (controller app language)
         );
       },
     ),
